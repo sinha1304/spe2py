c3836b6e433daf95cf09261be812713779806170
Shubham Sinha <66289917+sinha1304@users.noreply.github.com>
2025-03-10 12:13:11 -0400

    Update spe2py.py

--- spe2py.py
+++ spe2py.py
@@ -54,7 +54,7 @@ def spectra_to_txt(loaded_files, filenames,laser_wavelength):
             # Vectorized computation of Raman shifts and data points
             raman_shifts = raman_shift(obj.wavelength,laser_wavelength=laser_wavelength)
             #data_points = obj.data[0][0][50,:]
-            #data_points = np.mean(obj.data[:, 0, :], axis=0)
+            data_points = np.mean(obj.data[:, 0, :], axis=0)
             # Save data to .txt file
             np.savetxt(txt_filename, np.column_stack((raman_shifts, data_points)), 
                        header="Raman Shift (cm⁻¹)\tIntensity", comments='', delimiter='\t')
